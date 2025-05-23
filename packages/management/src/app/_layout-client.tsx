2153055f63605a11bd7ee22c3153f45ec593f2ee
debuggingdan <git@debuggingdan.com>
2025-05-23 13:30:42 +0200

    feat: added animated background, toggle for dark/light mode

--- packages/management/src/app/_layout-client.tsx
+++ packages/management/src/app/_layout-client.tsx
@@ -4,19 +4,34 @@ import '@mantine/notifications/styles.css';
 import '@lib/style/main.css';
 import { MantineProvider, ColorSchemeScript, createTheme } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
+import { useEffect, useState } from 'react';
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
 export function LayoutClient({ children }: { children: React.ReactNode }) {
+  const [colorScheme, setColorScheme] = useState<'light' | 'dark' | 'auto'>(
+    'auto',
+  );
+
+  useEffect(() => {
+    const storedColorScheme = localStorage.getItem('mantine-color-scheme') as
+      | 'light'
+      | 'dark'
+      | null;
+    if (storedColorScheme) {
+      setColorScheme(storedColorScheme);
+    }
+  }, []);
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider theme={theme} defaultColorScheme="auto">
+        <MantineProvider theme={theme} defaultColorScheme={colorScheme}>
           <Notifications />
           {children}
         </MantineProvider>
